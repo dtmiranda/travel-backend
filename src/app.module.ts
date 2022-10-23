0c0abb7de2fcf07415e282b65cfc2c016a8fe127
Sam <posix@pop-os.localdomain>
2022-10-23 19:07:56 -0100

    add event resource bootstrap

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { EventsModule } from './events/events.module';
 
 @Module({
-  imports: [UsersModule],
+  imports: [UsersModule, EventsModule],
   controllers: [AppController],
   providers: [AppService],
 })
