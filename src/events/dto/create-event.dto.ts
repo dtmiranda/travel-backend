ce297840eae421b84c87905bc01fca7b8cd2df65
dtmiranda <hortadito2@gmail.com>
2022-10-23 20:01:37 -0100

    add events migration

--- src/events/dto/create-event.dto.ts
+++ src/events/dto/create-event.dto.ts
@@ -1,1 +1,12 @@
-export class CreateEventDto {}
+import { Timestamp } from "rxjs";
+
+export class CreateEventDto {
+    id: string;
+    name: string;
+    description: string;
+    price: number;
+    start_date: string;
+    end_date: string;
+    event_hour: string;
+    event_coodenate: string;
+}
