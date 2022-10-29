627394fb73cef7b8e6072ad40387cd89476d76a8
dtmiranda <hortadito2@gmail.com>
2022-10-29 21:16:25 -0100

    add events resource

--- src/events/entities/event.entity.ts
+++ src/events/entities/event.entity.ts
@@ -1,1 +1,12 @@
-export class Event {}
+import { Prisma } from "@prisma/client";
+
+export class Event implements Prisma.EventUncheckedCreateInput{
+    id?: string;
+    name: string;
+    description: string;
+    price: number;
+    start_date: string;
+    end_date: string;
+    event_hour: string;
+    event_coodenate: string;
+}
--- src/events/events.service.ts
+++ src/events/events.service.ts
@@ -1,26 +1,43 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
 import { CreateEventDto } from './dto/create-event.dto';
 import { UpdateEventDto } from './dto/update-event.dto';
 
 @Injectable()
 export class EventsService {
+
+  constructor (private readonly prisma: PrismaService){}
+
   create(createEventDto: CreateEventDto) {
-    return 'This action adds a new event';
+    return this.prisma.event.create({
+      data:{
+        ...createEventDto
+      }
+    });
   }
 
   findAll() {
-    return `This action returns all events`;
+    return this.prisma.event.findMany();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} event`;
+  findOne(id: string) {
+    return this.prisma.event.findUnique({
+      where:{
+        id
+      }
+    });
   }
 
-  update(id: number, updateEventDto: UpdateEventDto) {
-    return `This action updates a #${id} event`;
+  update(id: string, updateEventDto: UpdateEventDto) {
+    return this.prisma.event.update({
+      where:{ id },
+      data: updateEventDto
+    });
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} event`;
+  remove(id: string) {
+    return this.prisma.event.delete({
+      where:{ id }
+    });
   }
 }
