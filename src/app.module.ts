499b73156face3d7537c99adab7c9d45b930faa5
dtmiranda <hortadito2@gmail.com>
2022-10-30 16:58:37 -0100

    add profiles resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,11 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { EventsModule } from './events/events.module';
+import { ProfilesModule } from './profiles/profiles.module';
+import { ProfilesModule } from './profiles/profiles.module';
 
 @Module({
-  imports: [UsersModule, EventsModule],
+  imports: [UsersModule, EventsModule, ProfilesModule],
   controllers: [AppController],
   providers: [AppService],
 })
