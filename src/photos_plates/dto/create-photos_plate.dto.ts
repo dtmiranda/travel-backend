0057b3e37e813f46c141deee56f5d068319a92f4
dtmiranda <hortadito2@gmail.com>
2022-11-05 08:22:51 -0100

    create photo room resource ans fix photos migrations

--- src/photos_plates/dto/create-photos_plate.dto.ts
+++ src/photos_plates/dto/create-photos_plate.dto.ts
@@ -2,9 +2,9 @@ import { Prisma } from "@prisma/client";
 
 export class CreatePhotosPlateDto implements Prisma.PhotoPlateCreateInput{
   id?: string;
-  link: string;
   plate: Prisma.PlateCreateNestedOneWithoutPhotos_platesInput;
+  photo: Prisma.PhotoCreateNestedOneWithoutPhotos_platesInput;
   create_at?: string | Date;
   update_at?: string | Date;
-  
+    
 }
--- src/photos_plates/entities/photos_plate.entity.ts
+++ src/photos_plates/entities/photos_plate.entity.ts
@@ -2,9 +2,9 @@ import { Prisma } from "@prisma/client";
 
 export class PhotosPlate implements Prisma.PhotoPlateUncheckedCreateInput{
   id?: string;
-  link: string;
   plateId: string;
+  photoId: string;
   create_at?: string | Date;
   update_at?: string | Date;
-  
+ 
 }
