4137d41b3dca9610b9e172243f9b196eae284b88
dtmiranda <hortadito2@gmail.com>
2023-03-29 11:42:07 -0100

    feat:using hashed password

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -2,15 +2,24 @@ import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class UsersService {
-  constructor(private readonly prisma: PrismaService) {}
+  constructor(private readonly prisma: PrismaService) { }
 
-  create(createUserDto: CreateUserDto) {
+  async create(createUserDto: CreateUserDto) {
+    const hashedPassword = await bcrypt.hash(createUserDto.password, 10);
+
+    console.log(hashedPassword)
+
     return this.prisma.user.create({
-      data:{
-        ...createUserDto
+
+      data: {
+        email: createUserDto.email,
+        password: hashedPassword,
+        username: createUserDto.username,
+
       }
     });
   }
@@ -21,7 +30,7 @@ export class UsersService {
 
   findOne(id: string) {
     return this.prisma.user.findUnique({
-      where:{
+      where: {
         id
       }
     });
@@ -29,14 +38,14 @@ export class UsersService {
 
   update(id: string, updateUserDto: UpdateUserDto) {
     return this.prisma.user.update({
-      where:{ id },
+      where: { id },
       data: updateUserDto
     });
   }
 
   remove(id: string) {
     return this.prisma.user.delete({
-      where:{ id }
+      where: { id }
     });
   }
 }
