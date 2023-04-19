2956e87e02313ddd2df1f715695b40accd15801e
dtmiranda <hortadito2@gmail.com>
2023-04-19 14:34:57 -0100

    change migation and add restaurant field validation

--- src/restaurants/dto/create-restaurant.dto.ts
+++ src/restaurants/dto/create-restaurant.dto.ts
@@ -1,20 +1,48 @@
 import { Prisma } from "@prisma/client";
+import { IsArray, IsEmail, IsNotEmpty, IsString } from "class-validator";
 
-export class CreateRestaurantDto implements Prisma.RestaurantCreateInput {
+export class CreateRestaurantDto implements Prisma.RestaurantUncheckedCreateInput {
   id?: string;
+
+  @IsNotEmpty()
+  @IsString()
   name: string;
+
+  @IsNotEmpty()
+  @IsString()
   description: string;
+
+  @IsNotEmpty()
+  @IsString()
   address: string;
+
+  @IsNotEmpty()
+  @IsString()
   whatsapp: string;
+
+  @IsEmail()
   email?: string;
+  @IsString()
   website?: string;
   rating: number;
+
+  @IsNotEmpty()
+  @IsString()
   amenities: string;
+
+  @IsNotEmpty()
+  @IsString()
   coordinate: string;
+
+  @IsNotEmpty()
+  @IsString()
   cuisine: string;
   menu: string;
   photos?: Prisma.PhotoCreateNestedManyWithoutRestaurantInput;
-  city: Prisma.CityCreateNestedOneWithoutRestaurantsInput;
+
+  @IsNotEmpty()
+  @IsString()
+  cityId: string;
   Review?: Prisma.ReviewCreateNestedManyWithoutRestaurantInput;
   create_at?: string | Date;
   update_at?: string | Date;
