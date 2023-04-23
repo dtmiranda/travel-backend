89df7420ce381878010376bd56765662d5ea6477
dtmiranda <hortadito2@gmail.com>
2023-04-23 21:17:18 -0100

    fix Cross-Origin Request Blocked error

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+const cors = require('cors')
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -12,6 +13,8 @@ async function bootstrap() {
       forbidNonWhitelisted: true,
     }),
   );
+
+  app.use(cors())
 
   await app.listen(5000);
 }
