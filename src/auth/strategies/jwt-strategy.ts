14c84998e6d3ba6f25c6cf46b788f72c5b051979
dtmiranda <hortadito2@gmail.com>
2023-04-23 21:16:47 -0100

    fix .env file recognize

--- src/auth/strategies/jwt-strategy.ts
+++ src/auth/strategies/jwt-strategy.ts
@@ -3,6 +3,7 @@ import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
 import { UserFromJwt } from '../models/UserFromJwt';
 import { UserPayload } from '../models/UserPayload';
+require('dotenv').config()
 
 @Injectable()
 export class JwtStrategy extends PassportStrategy(Strategy) {
